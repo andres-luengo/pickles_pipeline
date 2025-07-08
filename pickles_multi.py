9af392474da49cddc4fb2db899c74b54d81fc13d
andres-luengo <andres.luengo@yale.edu>
2025-07-08 12:22:48 -0700

    move memory limit to per-worker code

--- pickles_multi.py
+++ pickles_multi.py
@@ -14,14 +14,17 @@ if LOGS_PATH.is_dir():
     shutil.rmtree(LOGS_PATH)
 LOGS_PATH.mkdir()
 
-# resource management stuff. make lower to keep matt happier
 NUM_PROCESSES = 16 
 MAX_RSS = int(80 * 10**9) # so 80 GB
-resource.setrlimit(resource.RLIMIT_AS, (MAX_RSS,) * 2)
 
 NUM_BATCHES = 100 # this seems like the kind of temporary hack that is never really temporary...
 
 def pickles_worker(batch_num):
+    import pickles
+    import resource
+    MAX_MEMORY_PER_WORKER = MAX_RSS // NUM_PROCESSES
+    resource.setrlimit(resource.RLIMIT_AS, (MAX_MEMORY_PER_WORKER,) * 2)
+
     with open(LOGS_PATH / f'batch_{batch_num:>02}.log', 'w') as f:
         with contextlib.redirect_stdout(f):
             pickles.main(str(batch_num), 'True', 'False')
